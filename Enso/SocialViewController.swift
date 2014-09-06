3a60ad8f1b4cf2fbace28df64b45f56e47e62068
Leonardo Lee <lee.leonardo.king@gmail.com>
2014-09-06 13:46:28 -0700

    Implemented the basics for the Social Controller. I am going to figure out whats the best way to implement it with animations.

--- Enso/SocialViewController.swift
+++ Enso/SocialViewController.swift
@@ -7,20 +7,48 @@
 //
 
 import UIKit
+import Social
 
 class SocialViewController: UIViewController {
+    
+//    var interactionController = UIDocumentInteractionController()
+    var facebook = SLComposeViewController()
+    var twitter = SLComposeViewController()
+//    var instagram
+//    var pinterest
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupShareControllers()
+        
+        NSNotificationCenter.defaultCenter().addObserverForName("ReceiveHaiku", object: nil, queue: NSOperationQueue.mainQueue()) {
+            (note: NSNotification!) -> Void in
+        }
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReceiveHaiku", object: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    
+    func setupShareControllers() {
+        var notification = NSNotification(name: "ShareRequest", object: nil)
+        
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeFacebook) {
+            self.facebook = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+            self.facebook.setInitialText("Facebook initial text")
+            
+            //Check and add photos here.
+            
+        }
+        
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
+            self.twitter = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            self.twitter.setInitialText("Twitter initial text")
+        }
+    }
 
 }
