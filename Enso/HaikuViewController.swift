17e209410582aec6b7d3066de2b98f97fbcd6558
Leonardo Lee <lee.leonardo.king@gmail.com>
2014-09-06 00:19:07 -0700

    2 of 6 controllers' basic methods copied. I am going to copy the code over and then start working on implementing cleaner code. I am changing the photo controller to a media controller. Which is going to be a scroll View with a few different overlays.

--- Enso/HaikuViewController.swift
+++ Enso/HaikuViewController.swift
@@ -10,26 +10,52 @@ import UIKit
 
 class HaikuViewController: UIViewController {
 
+    @IBOutlet weak var haikuField: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.view.backgroundColor = UIColor(hue: 0, saturation: 0.5, brightness: 1, alpha: 0.1)
+        self.haikuField.backgroundColor = UIColor(hue: 0, saturation: 0.15, brightness: 0.7, alpha: 0.2)
+        
+        NSNotificationCenter.defaultCenter().addObserverForName(UITextViewTextDidChangeNotification, object: self, queue: NSOperationQueue.mainQueue()) { (note : NSNotification!) -> Void in
+        }
     }
-    */
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UITextViewTextDidChangeNotification, object: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 
+//MARK: Touch methods
+    override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!) {
+        for control in self.view.subviews {
+            if let theControl = control as? UITextView {
+                theControl.resignFirstResponder()
+            }
+        }
+    }
+    
+//MARK: HaikuController methods
+    func randomHaiku() {
+        var random = arc4random_uniform(10)
+        var randomHaiku : String
+        
+        switch random {
+        case 0:
+            println()
+        default:
+            println()
+        }
+    }
+    
+//MARK: UITextView
+    
 }
--- Enso/MainViewController.swift
+++ Enso/MainViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class MainViewController: UIViewController {
     
     var mainScrollView : UIScrollView!
+    var appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
                             
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +19,19 @@ class MainViewController: UIViewController {
         self.mainScrollView = UIScrollView(frame: self.view.frame)
         makeScrollView()
         
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        NSNotificationCenter.defaultCenter().addObserverForName("ShareRequest", object: self, queue: NSOperationQueue.mainQueue()) {
+            (note: NSNotification!) -> Void in
+            
+        }
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "ShareRequest", object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,6 +47,35 @@ class MainViewController: UIViewController {
         self.mainScrollView.bounces = true
         self.mainScrollView.alwaysBounceHorizontal = true
         self.view.addSubview(self.mainScrollView)
+        
+        var haikuController = self.storyboard.instantiateViewControllerWithIdentifier("Write") as HaikuViewController
+        var mediaController = self.storyboard.instantiateViewControllerWithIdentifier("Media") as MediaViewController
+        var socialController = self.storyboard.instantiateViewControllerWithIdentifier("Social") as SocialViewController
+        
+        //Delegates
+        
+        var firstX = 0.0
+        var secondX = Int(self.view.frame.width)
+        var thirdX = Int(self.view.frame.width * 2)
+        var standardWidth = Int(self.view.frame.width)
+        var standardHeight = Int(self.view.frame.height)
+        
+//        haikuController.view.frame = CGRect(x: firstX, y: 0, width: standardWidth, height: standardHeight)
+        mediaController.view.frame = CGRect(x: secondX, y: 0, width: standardWidth, height: standardHeight)
+        socialController.view.frame = CGRect(x: thirdX, y: 0, width: standardWidth, height: standardHeight)
+        
+        
+        self.addChildViewController(haikuController)
+        self.addChildViewController(mediaController)
+        self.addChildViewController(socialController)
+        
+        self.mainScrollView.addSubview(haikuController.view)
+        self.mainScrollView.addSubview(mediaController.view)
+        self.mainScrollView.addSubview(socialController.view)
+        
+        haikuController.didMoveToParentViewController(self)
+        mediaController.didMoveToParentViewController(self)
+        socialController.didMoveToParentViewController(self)
         
         
     }
