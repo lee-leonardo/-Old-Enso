7b30c70ec93a7a9ba03cc70da408151ca720f051
Leonardo Lee <lee.leonardo.king@gmail.com>
2014-09-06 18:44:48 -0700

    1. Implemented the basics for the Share Extension. 2. Generated a ObjC category for the Haiku Data model.
    I still need to update the Storyboard, and make the changes I've been thinking about:
    1. Change the scroll view. The Social is going to be an overlay that appears with a UIGesture.
    2. Media is going to have various features rather than just photo. I am going to have to think about a way to implement it effectively.
    
    Halt:
    1. Photo extension as my two or three effects doesn't justify creating it at the moment.
    2. Document Provider Extension, not because it wouldn't be awesome but because it would be a great 1.1 feature.

---
+++ Enso/Haiku+HaikuExtension.h
@@ -0,0 +1,13 @@
+//
+//  Haiku+HaikuExtension.h
+//  Enso
+//
+//  Created by Leonardo Lee on 9/6/14.
+//  Copyright (c) 2014 Leonardo Lee. All rights reserved.
+//
+
+#import "Haiku.h"
+
+@interface Haiku (HaikuExtension)
+
+@end
---
+++ Enso/Haiku+HaikuExtension.m
@@ -0,0 +1,13 @@
+//
+//  Haiku+HaikuExtension.m
+//  Enso
+//
+//  Created by Leonardo Lee on 9/6/14.
+//  Copyright (c) 2014 Leonardo Lee. All rights reserved.
+//
+
+#import "Haiku+HaikuExtension.h"
+
+@implementation Haiku (HaikuExtension)
+
+@end
